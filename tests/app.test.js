5edaffb3ea0e38484ccc4e8212617839d4267f02
Amit Kumar <kamit17@outlook.com>
2025-02-11 22:16:57 -0600

    Updated jest test to properly start/stop a test server

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,20 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app');  // Import the app, don't start a new server
 
-test('Health Check', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
+describe('API Tests', () => {
+    let server;
+
+    beforeAll((done) => {
+        server = app.listen(4000, done);  // Start test server on port 4000
+    });
+
+    afterAll((done) => {
+        server.close(done);  // Close the server after tests
+    });
+
+    test('Health Check', async () => {
+        const res = await request(server).get('/');
+        expect(res.statusCode).toBe(200);
+    });
 });
 
